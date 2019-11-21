d01597e93254cb91345847e260c6436639f13579
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-11-21 16:35:46 -0600

    Finished the top part of the page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../fonts/BOOKmanOpti-Bold.ttf";
+import "../fonts/LidoStfCondCe-Bold.ttf";
 import "./Navbar.css";
 //import { Link } from "react-router-dom";
 // <Link to="/" className="navbar-brand">
@@ -13,22 +14,49 @@ function Navbar() {
         id="gameNav"
         className="navbar fixed-top navbar-expand-md navbar-dark"
       >
-        <a id="gameBrand" className="navbar-brand" href="/">
-          <img id="bebopLogo" src="cowboy-bebop-logo.png" alt="cowboy-bebop-logo" />
+        <a id="gameBrand" className="navbar-brand pr-auto" href="/">
+          <img
+            id="bebopLogo"
+            src="cowboy-bebop-logo.png"
+            alt="cowboy-bebop-logo"
+          />
           <br />
-          <h5 id="brandSubtitle" className="text-white text-center">THE MEMORY GAME</h5>
         </a>
 
-        <ul className="navbar-nav ml-auto">
+
+        <ul className="navbar-nav mx-auto">
           <li className="nav-item">
-            <h5 className="text-white">Score: 0 | Top Score: 10</h5>
+            <h5 id="brandSubtitle" className="text-white text-center mb-0">
+              THE MEMORY GAME
+            </h5>
+          </li>
+        </ul>
+
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item ml-auto">
+            <h5 className="text-white mb-0 pr-2">Score: 0 | Top Score: 10</h5>
           </li>
         </ul>
       </nav>
 
-      <img id="bebopImage" src="cowboy-bebop-banner.jpg" alt="cowboy-bebop-banner" />
+      <img
+        id="bebopImage"
+        src="cowboy-bebop-banner.jpg"
+        alt="cowboy-bebop-banner"
+      />
 
-      <div id="gameInstructions"></div>
+      <div
+        id="gameInstructions"
+        className="d-flex align-items-center justify-content-end"
+      >
+        <div className="text-right pr-5">
+          <h4 className="text-white mb-0">
+            CLICK ON AN IMAGE BELOW TO EARN POINTS, BUT
+          </h4>
+
+          <h4 className="text-white mb-0">DON'T CLICK ON ANY MORE THAN ONCE...</h4>
+        </div>
+      </div>
     </div>
   );
 }
