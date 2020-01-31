e8a49be1ef33c2a732c56152e1c795f53fe98ffb
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2020-01-31 13:32:49 -0600

    Added new text to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer className="stickyFooter mt-auto d-flex align-items-center justify-content-center">
-            <p className="mb-0"></p>
+            <p className="mb-0">Game developed by Antonio de las Morenas for non-commercial portfolio use only. Rights for all images go to their respective owners. No copyright infringement intended.</p>
         </footer>
     );
 }
